99da92084d4f7afb937cf807c5bb492bce5e4a08
Koneke <lh.koneke@gmail.com>
2024-12-04 15:47:51 +0100

    cleaning

--- day2/day2.ml
+++ day2/day2.ml
@@ -4,16 +4,13 @@ let is_asc_or_desc l =
   l = (List.sort compare l)
   || (List.rev l) = (List.sort compare l)
 
-let rec is_in_bounds l =
+let rec is_gradual l =
   match l with
   | [] -> true
   | x :: [] -> true
-  | x :: y :: xs -> x <> y && (abs (x - y)) < 4 && is_in_bounds (y :: xs)
+  | x :: y :: xs -> x <> y && (abs (x - y)) < 4 && is_gradual (y :: xs)
 
-let is_safe l =
-  let is_straight = is_asc_or_desc l in
-  let is_gradual = is_in_bounds l in
-  is_straight && is_gradual
+let is_safe l = is_asc_or_desc l && is_gradual l
 
 let rec except n l =
   match l with
@@ -21,19 +18,6 @@ let rec except n l =
   | x :: xs -> if n = 0 then xs else (x :: (except (n - 1) (xs)))
 
 let is_safeish l =
-(*
-  let sublists = List.init (List.length l) (fun x -> except x l) in
-  List.iter print_endline (List.map string_of_bool (List.map is_safe sublists));
-
-  print_endline "";
-
-  (fun xs -> print_endline (String.concat ", " (List.map string_of_int xs)))
-
-  print_endline
-    ((String.concat ", "
-    (List.map string_of_int (List.init (List.length l) (fun x -> except x l)))));
-*)
-
   List.fold_left
     (||)
     false
@@ -61,10 +45,5 @@ let () =
   let input = read_input in
   let safes = (List.map is_safe input) in
   let safeishs = (List.map is_safeish input) in
-  (*
-  let sample = [1;2;7;8;9] in
-  print_endline (string_of_bool (is_safeish sample));
-  print_endline (String.concat "," (List.map string_of_int (except 3 sample)));
-  *)
   print_endline (string_of_int (List.length (List.filter (fun x -> x) safes)));
   print_endline (string_of_int (List.length (List.filter (fun x -> x) safeishs)))
